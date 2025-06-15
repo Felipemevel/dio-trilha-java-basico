6855907f125c390aa9b34cf165f21f30dbd547cb
Felipe <lipe.conta1632@gmail.com>
2025-06-14 21:49:40 -0300

    Finalização do desafio sobre a apresentação de uma conta bancária

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //TODO:
+        // Importação da classe scanner
+        // Exibição de mensagens
+        // Obter dados pelo scanner
+        // Criar condicionais para verificação
+        // Exibir dados inseridos
+
+        var scanner = new Scanner (System.in);
+        System.out.println("Olá! Seja bem vindo ao Santander! Por favor, nos forneça os dados abaixo.");
+
+        System.out.println("Qual é o seu nome e sobrenome?");
+        var nome = scanner.nextLine();
+
+        System.out.println("Agência: ");
+
+        int agencia = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Conta: ");
+        String conta = scanner.nextLine();
+
+        double saldo = 400.00;
+
+        if (String.valueOf(agencia).length() < 3 || String.valueOf(agencia).length() >= 5) System.out.println("Agência inválida.");
+
+        if (conta.length() != 5) System.out.println("Conta inválida.");
+
+        System.out.printf("Olá, %s! Muitas boas vindas ao Santander! Esses são os dados da sua conta:\nAgência: %s\nConta: %s \nSaldo disponível para saque: R$ %.2f", nome, agencia, conta, saldo);
+
+
+        scanner.close();
     }
 }
